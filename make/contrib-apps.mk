d7ff938b77cc18ff6749af25ee1bba24db8fbd51
Audioniek <audioniek@xs4all.nl>
2017-08-05 22:18:35 +0200

    update build ntfs-3g

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -498,7 +498,7 @@ $(D)/ntfs-3g: $(D)/bootstrap $(ARCHIVE)/$(NTFS_3G_SOURCE)
 	$(REMOVE)/ntfs-3g_ntfsprogs-$(NTFS_3G_VER)
 	$(UNTAR)/$(NTFS_3G_SOURCE)
 	$(SILENT)set -e; cd $(BUILD_TMP)/ntfs-3g_ntfsprogs-$(NTFS_3G_VER); \
-		CFLAGS="-pipe -O2 -g" $(CONFIGURE) \
+		CFLAGS="-pipe -Os -g" $(CONFIGURE) \
 			--build=$(BUILD) \
 			--host=$(TARGET) \
 			--prefix=/usr \
@@ -509,7 +509,6 @@ $(D)/ntfs-3g: $(D)/bootstrap $(ARCHIVE)/$(NTFS_3G_SOURCE)
 			--disable-ldconfig \
 			--disable-ntfsprogs \
 			--disable-static \
-			--with-fuse=internal \
 			--enable-silent-rules \
 		; \
 		$(MAKE); \
