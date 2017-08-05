a41fb3f1c67d5db72056f6fe799fd742ba3fcf30
Audioniek <audioniek@xs4all.nl>
2017-08-05 22:04:36 +0200

    update build sysvinit

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -258,6 +258,7 @@ $(D)/sysvinit: $(D)/bootstrap $(ARCHIVE)/sysvinit_$(SYSVINIT_VER).orig.tar.gz
 		$(MAKE) -C src SULOGINLIBS=-lcrypt; \
 		$(MAKE) install ROOT=$(TARGETPREFIX) MANDIR=/.remove
 	rm -f $(addprefix $(TARGETPREFIX)/sbin/,fstab-decode runlevel telinit)
+	rm -f $(addprefix $(TARGETPREFIX)/usr/bin/,lastb
 ifeq ($(BOXTYPE), $(filter $(BOXTYPE), fortis_hdbox octagon1008 cuberevo cuberevo_mini2 cuberevo_2000hd))
 	$(SILENT)install -m 644 $(SKEL_ROOT)/etc/inittab_ttyAS1 $(TARGETPREFIX)/etc/inittab
 else
